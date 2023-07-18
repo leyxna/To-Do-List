e2d785f6f65c374643a6c70ab5a80825374120e3
Leyna <leynaphan16@gmail.com>
2023-07-18 12:02:16 -0500

    Complete Iteration 0

---
+++ To Do List/ToDoItem.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoItem.swift
+//  To Do List
+//
+//  Created by Leyna Phan on 7/18/23.
+//
+
+import Foundation
+
+class ToDoItem {
+    
+    var title = ""
+    var isImportant = false
+    
+    init(title: String, isImportant: Bool = false) {
+            self.title = title
+            self.isImportant = isImportant
+        }
+}
