74f879cf551e20d204d8498d06bb2b3b4ed6182c
Leyna <leynaphan16@gmail.com>
2023-07-19 08:38:34 -0500

    Complete Iteration 1.

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -8,29 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var toDoItems: [ToDoItem] = []
+    @State private var showNewTask = false
     var body: some View {
         VStack {
             HStack {
                 Text("To Do List")
                     .font(.system(size:40))
-                         .fontWeight(.black)
+                    .fontWeight(.black)
                 Spacer()
                 Button(action: {
-                                    
+                    withAnimation {
+                        self.showNewTask = true
+                    }
                 }) {
-                Text("+")
+                    Text("+")
                         .font(.system(size:30))
                 }
-                
             }
             .padding()
             Spacer()
-                
             
+            
+            
+            List {
+                ForEach (toDoItems) { toDoItem in if toDoItem.isImportant == true {
+                    Text("‼️" + toDoItem.title)
+                } else {
+                    Text(toDoItem.title)
+                }}
+            }
+            .scrollContentBackground(.hidden)
+            .listStyle(.plain)
+            
+            if showNewTask {
+                NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+                    .shadow(radius: 2)
+                    .transition(.move(edge: .bottom))
+            }
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- To Do List/NewToDoView.swift
+++ To Do List/NewToDoView.swift
@@ -10,35 +10,56 @@ import SwiftUI
 struct NewToDoView: View {
     @State var title: String
     @State var isImportant: Bool
+    @Binding var toDoItems: [ToDoItem]
+    @Binding var showNewTask : Bool
+    
     var body: some View {
-        VStack {
-            Text("Task title:")
-                .font(.title)
-                .fontWeight(.bold)
-            TextField("Enter the task description...", text: $title)
+        ZStack {
+            Color(hue: 0.962, saturation: 0.5, brightness: 0.95)
+                .ignoresSafeArea()
+            VStack {
+                Text("Task title:")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                TextField("Enter the task description...", text: $title)
                     .padding()
-                .background(Color(.systemGroupedBackground))
-                .cornerRadius(15)
-                  .padding()
-                            
-            Toggle(isOn: $isImportant) { Text("Is it important?")
-            } .padding()
+                    .background(Color(.systemGroupedBackground))
+                    .cornerRadius(15)
+                    .padding()
+                
+                Toggle(isOn: $isImportant) { Text("Is it important?")
+                        .foregroundColor(Color.white)
+                } .padding()
                 
                 Button(action: {
-                  
+                    self.addTask(title: self.title, isImportant: self.isImportant)
+                    self.showNewTask = false
                 }) {
                     Text("Add")
-                        .padding()
+                        .padding(.horizontal, 160)
+                        .padding(.vertical, 15)
+                        .background(Color(.systemGroupedBackground))
+                        .cornerRadius(18)
                         
+                
                 }
             }
             
+        }
+    }
+    private func addTask(title: String, isImportant: Bool = false) {
+            
+            let task = ToDoItem(title: title, isImportant: isImportant)
+            toDoItems.append(task)
         }
     }
 
 
 struct NewToDoView_Previews: PreviewProvider {
     static var previews: some View {
-        NewToDoView(title:"", isImportant: false)
+        NewToDoView(title:"", isImportant: false, toDoItems: .constant([]), showNewTask: .constant(true))
+    
     }
 }
+
